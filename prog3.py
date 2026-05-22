0d8899f0af4a747ba807f88fc6d866e0d90768cc
ankitamundra88-pixel <ankitamundra88@gmail.com>
2026-05-22 17:49:26 +1000

    Add files via upload

---
+++ prog3.py
@@ -0,0 +1,13 @@
+# creating diffrent variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+# user input
+name = input("Enter your name")
+
+print("\nhello", name, "\nWelcome to codingal")
---
+++ prog4.py
@@ -0,0 +1,3 @@
+name = "Aarav"
+Age = 10
+print("My name is Aarav and i am 10 years old")
