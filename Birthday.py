577c63fe053e70a1da6c329d12b479d2f6c41d06
ankitamundra88-pixel <ankitamundra88@gmail.com>
2026-05-25 18:45:48 +1000

    Add files via upload

---
+++ Birthday.py
@@ -0,0 +1,35 @@
+print("Raj's Birthday Book")
+print("1.Show all Birthdays")
+print("2.Check today's Birthdays")
+print("3.Show next upcoming birthday")
+print("4.Add a friend")
+print("5.quit")
+
+print("Choose number 1 to 5")
+
+print("1. Show all Birthdays")
+
+print("Aarav's Birthday is on 12th January")
+print("Charlie's Birthday is on 23rd February")
+print("Sophia's Birthday is on 5th March")
+print("Mia's Birthday is on 17th April")
+print("Liam's Birthday is on 30th May")
+
+print("2. Check today's Birthdays")
+
+print("Liam's Birthday is today! Happy Birthday, Liam!")
+
+print("3. Show next upcoming birthday")
+
+print("Aarav's Birthday is the next upcoming birthday on 12th January")
+
+print("4. Add a friend")
+
+print("Enter your friend's name:")
+friend_name = input()
+print("Enter your friend's birthday (e.g., 15th August):")
+friend_birthday = input()
+print(f"{friend_name}'s Birthday on {friend_birthday} has been added to the Birthday Book.")
+
+print("5. Quit")
+print("Thank you for using Raj's Birthday Book! Goodbye!")
