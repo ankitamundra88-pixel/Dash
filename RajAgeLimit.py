3ebc9c99110794597f0ae4d9e30058fee0787263
ankitamundra88-pixel <ankitamundra88@gmail.com>
2026-07-05 19:15:51 +1000

    Add files via upload

---
+++ RajAgeLimit.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+if age <= 20:
+    print("You are allowed")
+else:
+    print("You are not allowed")
+if age <= 10:
+    print("you are allowed")
+else:
+    print("you are not allowed")
